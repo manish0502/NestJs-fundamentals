56e298cd397ac7474fbf28f1e86fe81db53a7c3b
manish0502 <manishgoswami495@gmail.com>
2021-12-09 13:57:47 +0530

    dto added with controllers

--- car-pricing-project/src/users/users.controller.ts
+++ car-pricing-project/src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body ,Controller  ,Post ,Get} from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+
+@Controller('auth')
+export class UsersController {
+
+
+    //http://localhost:3000/auth/signup
+    
+    @Post('/signup')
+    createUser(@Body() body: CreateUserDto){
+         console.log(body)
+    }
+}
