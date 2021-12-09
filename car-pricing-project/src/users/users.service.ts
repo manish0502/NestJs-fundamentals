08879cb416894c1df5bdee24c9c767ca8c80a153
manish0502 <manishgoswami495@gmail.com>
2021-12-09 19:47:52 +0530

    added notFoundException

--- car-pricing-project/src/users/users.service.ts
+++ car-pricing-project/src/users/users.service.ts
@@ -37,7 +37,7 @@ export class UsersService {
   async remove(id: number) {
     const user = await this.findOne(id);
     if (!user) {
-      throw new Error('user not found');
+      throw new NotFoundException('user not found');
     }
     return this.repo.remove(user);
   }
