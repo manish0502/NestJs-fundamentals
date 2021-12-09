f99022d7a5b76e173a6bf6576b30cf1b11c17195
manish0502 <manishgoswami495@gmail.com>
2021-12-09 15:02:11 +0530

    signup works and saved data

--- car-pricing-project/src/users/users.controller.ts
+++ car-pricing-project/src/users/users.controller.ts
@@ -1,15 +1,18 @@
 import { Body ,Controller  ,Post ,Get} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
-
+import { UsersService } from './users.service'
 
 @Controller('auth')
 export class UsersController {
+
+
+    constructor(private userService: UsersService){}
 
 
     //http://localhost:3000/auth/signup
     
     @Post('/signup')
     createUser(@Body() body: CreateUserDto){
-         console.log(body)
+         this.userService.create(body.email , body.password)
     }
 }
--- car-pricing-project/src/users/users.module.ts
+++ car-pricing-project/src/users/users.module.ts
@@ -9,6 +9,6 @@ import { User } from './user.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
