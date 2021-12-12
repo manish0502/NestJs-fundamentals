8e004a2ee36a237e4d8bf2163625fe722dd322a7
manish0502 <manishgoswami495@gmail.com>
2021-12-12 16:18:16 +0530

    added enviroment variables

--- car-pricing-project/src/main.ts
+++ car-pricing-project/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+//import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 //const cookieSession = require('cookie-session');
 import * as session from 'express-session';
@@ -13,19 +13,19 @@ async function bootstrap() {
   //   }),
   // );
 
-  app.use(
-    session({
-      secret: 'keyboard cat',
-      resave: false,
-      saveUninitialized: true,
-      cookie: { maxAge: 60000 }
-    }),
-  );
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true,
-    }),
-  );
+  // app.use(
+  //   session({
+  //     secret: 'keyboard cat',
+  //     resave: false,
+  //     saveUninitialized: true,
+  //     cookie: { maxAge: 60000 }
+  //   }),
+  // );
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true,
+  //   }),
+  // );
   (app as any).set('etag', false);
   app.use((req, res, next) => {
     res.removeHeader('x-powered-by');
