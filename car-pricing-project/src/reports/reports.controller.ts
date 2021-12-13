e42f20846aa3aa03e9901263b8ca3ee46674bc62
manish0502 <manishgoswami495@gmail.com>
2021-12-13 17:41:19 +0530

    some changes

--- car-pricing-project/src/reports/reports.controller.ts
+++ car-pricing-project/src/reports/reports.controller.ts
@@ -24,7 +24,7 @@ export class ReportsController {
 
 
   @Patch('/:id')
-  @UseGuards(AdminGuard)
+  // @UseGuards(AdminGuard)
   approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
   }
