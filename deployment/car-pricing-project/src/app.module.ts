fadd44f95b31694ee922396193d8fd5b0bc5931e
manish0502 <manishgoswami495@gmail.com>
2021-12-14 12:05:15 +0530

    env setup for production deployment

--- deployment/car-pricing-project/src/app.module.ts
+++ deployment/car-pricing-project/src/app.module.ts
@@ -17,24 +17,7 @@ const cookieSession = require('cookie-session');
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
-    TypeOrmModule.forRootAsync({
-      inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        return {
-          type: 'sqlite',
-          database: config.get<string>('DB_NAME'),
-          synchronize: true,
-          entities: [User, Report],
-        };
-      },
-    }),
-    // TypeOrmModule.forRoot(
-    //   {
-    //     type: 'sqlite',
-    //     database: 'db.sqlite',
-    //     entities: [User , Report],
-    //     synchronize: true,
-    //   }),
+    TypeOrmModule.forRoot(),
     UsersModule,
     ReportsModule,
   ],
@@ -51,18 +34,6 @@ const cookieSession = require('cookie-session');
 })
 export class AppModule {
   constructor(private configService: ConfigService) {}
-  // configure(consumer: MiddlewareConsumer) {
-  //   consumer
-  //     .apply(
-  //       session({
-  //         secret: 'keyboard cat',
-  //         resave: false,
-  //         saveUninitialized: true,
-  //         cookie: { maxAge: 60000 }
-  //       })
-  //     )
-  //     .forRoutes('*');
-  // }
 
   configure(consumer: MiddlewareConsumer) {
     consumer
